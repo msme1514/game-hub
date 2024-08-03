9e86e1ff375fce48346a416b8bb5de66e9428d9b
Mustafa Mohammed <mmus1514@gmail.com>
2024-08-03 08:35:01 -0700

    Getting started with Game-hub, Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { useEffect, useState } from "react";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import axios, { CanceledError } from "axios";
 // import { categories } from "./expense-tracker/categories";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 interface User {
   id: number;
@@ -11,30 +7,9 @@ interface User {
 }
 
 const App = () => {
-  const [users, setUsers] = useState<User[]>();
-  const [error, setError] = useState("");
-  useEffect(() => {
-    const controller = new AbortController();
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
-        signal: controller.signal,
-      })
-      .then((res) => setUsers(res.data))
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-      });
-
-    return () => controller.abort();
-  }, []);
   return (
     <>
-      {error && <p className="text-danger">{error}</p>}
-      <ul>
-        {users?.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <Button colorScheme="blue">Button</Button>
     </>
   );
 };
