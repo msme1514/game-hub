a5b47020c664aa663589c0b5ff2296bbe56ab426
Mustafa Mohammed <mmus1514@gmail.com>
2024-08-05 08:39:52 -0700

    Display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Badge, Text } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <>
+      <Badge
+        colorScheme={color}
+        fontSize="14px"
+        paddingX={2}
+        borderRadius="4px"
+      >
+        {score}
+      </Badge>
+    </>
+  );
+};
+
+export default CriticScore;
